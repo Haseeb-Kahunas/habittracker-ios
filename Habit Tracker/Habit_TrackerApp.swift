a8a90eeed0d13c634289a5f7e34f2c807f4884b6
Haseeb Ahmed <Haseeb@kahunas.io>
2025-10-06 18:56:03 +0400

    Initial Commit

---
+++ Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Haseeb Ahmed on 06/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
